1065a66c014341d5f187db7514da40f679327657
BigMoj <mojthegreat@gmail.com>
2023-02-22 12:52:13 +0330

    finished

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
+from turtle import Screen
+import time
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Crossing Game")
+screen.tracer(0)
+
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+
+
+screen.listen()
+screen.onkey(player.move, "w")
+
+
+
+
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.ycor() >= 280:
+        scoreboard.level_up()
+        player.reset_position()
+        car_manager.move_speed()
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
